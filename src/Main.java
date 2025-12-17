07d069d23ee517214a71b098c4638741118daa86
Liudmila <milalindley@gmail.com>
2025-12-17 19:31:44 +0100

    feat: use TreeSet

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
+        List<Employee> employees = readFile().stream()
+                .filter(e -> !e.isBlank())
+                .map(str -> {
+                    String[] arr = str.trim().split(" ");
+                    return new Employee(arr[0], arr[1]);
+                }).toList();
 
+        System.out.println("Size of collection: " + employees.size());
+
+        System.out.println("\nPrint employees data in alphabetical order:");
+        employees.forEach(System.out::println);
+    }
+
+    private static TreeSet<String> readFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader("resources/employees.txt"))) {
+            var list = br.lines().toList();
+            TreeSet<String> ts = new TreeSet<>(list);
+            System.out.println("Size of TreeSet: " + ts.size());
+            return ts;
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file.");
+        }
     }
 }
